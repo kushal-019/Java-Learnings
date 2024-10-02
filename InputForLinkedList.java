eb295422b13bce7d212f28148243294fe1b0d3dc
kushal goel <kgoel8451@gmail.com>
2024-10-02 23:08:27 +0530

    Linked list

---
+++ InputForLinkedList.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class InputForLinkedList{
+    public static void main(String args[]){
+        int n;
+        Scanner scan = new Scanner(System.in);
+
+        LinkedList myList1 = new LinkedList();
+        LinkedList myList2 = new LinkedList();
+
+        n  = scan.nextInt();
+
+        for(int i=0;i<n;i++){
+            int ele = scan.nextInt();
+            myList1.addFirst(ele);
+            myList2.addLast(ele);
+        }
+
+        myList1.printList();
+        myList2.printList();
+    }
+}
---
+++ LinkedList.java
@@ -0,0 +1,74 @@
+import java.util.*;
+
+class LinkedList{
+
+    Node head;
+    private int size;
+
+
+    LinkedList(){
+        this.size = 0 ;
+    }
+
+
+    class Node{
+        int data;
+        Node next;
+
+        Node(int val){
+            this.next = null;
+            this.data = val;
+            size++;
+        }
+
+    }
+
+    void addFirst(int val){
+        Node temp = new Node(val);
+
+        if(head != null)temp.next = head;
+
+            head = temp;
+    }
+
+    void addLast(int val){
+        Node temp = head;
+
+        if(head == null)head = new Node(val);
+        else{
+            while(temp.next != null)temp = temp.next;
+
+            temp.next = new Node(val);
+        }
+
+    }
+
+    void removeLast(){
+        Node temp =  head;
+
+        if(temp.next == null)head = null;
+        else {
+            while(temp.next != null)temp = temp.next;
+            temp = null;
+        }
+    }
+
+    void removeFirst(){
+        if(head.next == null)head = null;
+        else {
+            head = head.next;
+        }
+    }
+
+    void printList(){
+        Node temp = head;
+
+        while(temp != null){
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+
+        System.out.println();
+    }
+
+}
